1ce961b7b9593a0c8bf19a878c3d25f8d49fe4e2
Cuize <cuize@9801a7a2255d.ant.amazon.com>
2018-10-10 23:55:40 -0700

    fix too big penalty problem

--- shared/TreeNode.cpp
+++ shared/TreeNode.cpp
@@ -416,7 +416,7 @@ bool CTreeNode::setSplit(double nodeV, double nodeSum, double squares, double mu
 			//there is exactly one split for a boolean attribute, evaluate it
 			SplitInfo boolSplit(attr, 0.5);
 			double sqErrRatioBool = evalBool(boolSplit, nodeV, nodeSum, squares);
-			double eval = sqErrRatioBool + penalty/(1 - sqErrRatioBool);
+			double eval = sqErrRatioBool + min(penalty/(1 - sqErrRatioBool),1);
 			if(isnan(eval))
 			{//boolean attribute is not valid anymore, remove it
 				pAttrs->erase(pAttrs->begin() + attrNo);	
@@ -509,7 +509,7 @@ bool CTreeNode::setSplit(double nodeV, double nodeSum, double squares, double mu
 
 					double sqErrRatio = ( sqErr1 + sqErr2 + squares )/variance;
 
-					double eval = sqErrRatio + penalty/(1 - sqErrRatio) ;
+					double eval = sqErrRatio + min(penalty/(1 - sqErrRatio),1) ;
 			
 					//evaluate the split point, if it is the best (one of the best) so far, keep it
 					if(isnan(bestEval) || (eval < bestEval))
