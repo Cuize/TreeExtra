61a9f0ad5838744a90329a04232f56601659cd70
Cuize <cuize@9801a7a2255d.ant.amazon.com>
2018-12-07 17:50:57 -0800

    check point before further optimization

--- shared/TreeNode.cpp
+++ shared/TreeNode.cpp
@@ -222,8 +222,8 @@ bool CTreeNode::split(double alpha, double rootVar, double* pEntropy, double mu,
 	double comp1 = d0;
 	double comp2 =( 3 + max(1.0,log(n))) * log2(d);
 	// bool trigger = (( remain == 1 ) && (  comp2 < comp1 ));
-	// bool trigger = (remain == 1);
-	bool trigger = (( remain == 1 ) || ( (int)pSorted->size() == *numUsed));
+	bool trigger = (remain == 1);
+	// bool trigger = (( remain == 1 ) || ( (int)pSorted->size() == *numUsed));
 
 
 	// cout<< "group: " << group << endl;
